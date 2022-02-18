8febd3261a80de50ddef9353abef1d80b92a3dee
saltbo <saltbo@foxmail.com>
2022-02-18 17:10:31 +0800

    feat: add autoscripts

---
+++ .autoscripts/clock-in-after-work-notify.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+targetHour=18
+currentHour=`date +%H`
+if [ $currentHour -ge $targetHour ]
+then
+    curl https://api.day.app/WGqwegBts4P4FiEPF5HC7T/%E6%89%93%E5%8D%A1%E6%8F%90%E9%86%92/%E4%B8%8B%E7%8F%AD%E4%BA%86%EF%BC%9F%E8%AE%B0%E5%BE%97%E6%89%93%E5%8D%A1%EF%BC%81%EF%BC%81
+fi

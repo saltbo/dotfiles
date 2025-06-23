3b3106851d1357823065e2d77fa25aabb61eacf6
saltbo <saltbo@foxmail.com>
2025-06-23 20:11:34 +0800

    feat: first commit
    Signed-off-by: saltbo <saltbo@foxmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+## Install
+
+```bash
+curl -sfL https://raw.githubusercontent.com/saltbo/dotfiles/master/install.sh | bash
+```
---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -eufo pipefail
+
+echo "🚀  Setting up @saltbo dotfiles."
+
+if xcode-select -p &>/dev/null; then
+    echo "✅  Xcode command line tools are already installed."
+else
+    echo "🔧  Installing Xcode command line tools..."
+    xcode-select --install &>/dev/null
+
+    while ! xcode-select -p &>/dev/null; do
+        sleep 5
+    done
+    echo "✅  Xcode command line tools installed successfully."
+fi
+
+if which -s "brew"; then
+    echo "✅  Homebrew is already installed."
+else
+    echo "🍺  Installing Homebrew"
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "✅  Homebrew installed successfully."
+fi
+
+if [ -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
+    echo "✅  oh-my-zsh is already installed."
+else
+    echo "💻  Installing oh-my-zsh"
+    yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+    echo "✅  oh-my-zsh installed successfully."
+fi
+
+if which -s "chezmoi"; then
+    echo "✅  Chezmoi is already installed."
+else
+    echo "⚪️  Installing Chezmoi"
+    brew install chezmoi
+fi
+
+if [ -d "$HOME/.local/share/chezmoi/.git" ]; then
+    echo "ℹ️  Chezmoi already initialized, pulling latest changes..."
+    chezmoi update
+    echo "✅  Chezmoi updated"
+else
+    chezmoi init saltbo
+    chezmoi apply
+    echo "✅  Chezmoi initialized"
+fi
